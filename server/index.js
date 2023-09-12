90d81701d5d8cb8dae358cedda0f9ed07f3b21b1
Natalia Alvarez <122940112+natisalvarez@users.noreply.github.com>
2023-09-12 21:29:43 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT = 3002;
-const getApi = require ('../server/src/controllers/getApi');
+const getApi = require('../server/src/controllers/getApi');
 
+const PORT = process.env.PORT || 3002;
 conn.sync({ alter: true })
-.then( async() => {
-  await getApi();
+  .then(async () => {
+    await getApi();
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.error("Error during database synchronization:", error);
+    process.exit(1); 
+  });
