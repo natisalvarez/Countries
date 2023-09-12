9e578d6d7fdc262c4af8ab3ee805c5d1af10773b
Natalia Alvarez <122940112+natisalvarez@users.noreply.github.com>
2023-09-12 21:27:28 +0200

    Update getApi.js

--- server/src/controllers/getApi.js
+++ server/src/controllers/getApi.js
@@ -1,31 +1,18 @@
-const axios = require('axios');
-const { Country } = require('../db');
+const axios = require("axios");
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+const getApi = require('../server/src/controllers/getApi');
 
-const getApi = async () => {
-    const dbCountries = Country.findAll();
-    if (!dbCountries.length) {
-        const url = await axios.get("http://localhost:5000/countries");
-        let urlCleaned = url.data.map((country) => {
-            return {
-                id: country.cca3,
-                name: country.name.common,
-                image: country.flags.svg,
-                continent: country.continents[0],
-                capital: country.capital ? country.capital[0] : "Capital",
-                subregion: country.subregion ? country.subregion : "Subregion",
-                area: country.area,
-                population: country.population
-            };
-        });
+const PORT = process.env.PORT || 3002;
+conn.sync({ alter: true })
+  .then(async () => {
+    await getApi();
 
-        for (let i = 0; i < urlCleaned.length; i++) {
-            await Country.findOrCreate({
-                where: { name: urlCleaned[i].name },
-                defaults: urlCleaned[i],
-            });
-        }
-        console.log("La base de datos ha sido actualizada")
-    }
-};
-
-module.exports = getApi;
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.error("Error during database synchronization:", error);
+    process.exit(1); 
+  });
