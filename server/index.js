9ad1e3c9d3a660478974be643b35f3f2bdb23e0f
Natalia Alvarez <122940112+natisalvarez@users.noreply.github.com>
2023-09-16 15:50:59 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,34 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const getApi = require('../server/src/controllers/getApi.js');
-
 const PORT = process.env.PORT || 3002; 
+
+const getApi = async () => {
+    const dbCountries = Country.findAll();
+    if (!dbCountries.length) {
+        const url = await axios.get("http://localhost:5000/countries");
+        let urlCleaned = url.data.map((country) => {
+            return {
+                id: country.cca3,
+                name: country.name.common,
+                image: country.flags.svg,
+                continent: country.continents[0],
+                capital: country.capital ? country.capital[0] : "Capital",
+                subregion: country.subregion ? country.subregion : "Subregion",
+                area: country.area,
+                population: country.population
+            };
+        });
+
+        for (let i = 0; i < urlCleaned.length; i++) {
+            await Country.findOrCreate({
+                where: { name: urlCleaned[i].name },
+                defaults: urlCleaned[i],
+            });
+        }
+        console.log("La base de datos ha sido actualizada")
+    }
+};
 
 conn.sync({ alter: true })
   .then(async () => {
