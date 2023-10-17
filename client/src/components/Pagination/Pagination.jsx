c2d5465e4ef8a40231744ef91bdd8dde42803a75
Natalia Alvarez <naatisalvarez@gmail.com>
2023-10-17 09:59:38 +0200

    estilos

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -24,43 +24,67 @@ const Pagination = ({ itemsPerPage, totalItems, currentPage, onPageChange }) => 
       onPageChange(currentPage + 1);
     }
   };
-
   const handlePageClick = (page) => {
     onPageChange(page);
   };
 
+  const handleLastPage = () => {
+    onPageChange(totalPages);
+  };
   
-
   return (
-    <nav className={style.pagination}>
-      <a
-        href="#"
-        className={`${style.previousButton} ${!isFirstPage ? style.showButton : ""}`}
-        onClick={handlePrevPage}
-      >
-        Previous Page
-      </a>
-      <div className={style.pageNumber}>
-        {[...Array(totalPages)].map((_, index) => (
-          <a
-            key={index}
-            className={`${style.pageNumber} ${currentPage === index + 1 ? style.active : ""}`}
-            href="#"
-            onClick={() => handlePageClick(index + 1)}
-          >
-            {index + 1}
-          </a>
-        ))}
-      </div>
-      <a
-        href="#"
-        className={`${style.nextButton} ${!isLastPage ? style.showButton : ""}`}
-        onClick={handleNextPage}
-      >
-        Next Page
-      </a>
-    </nav>
-  );
-};
+        <nav className={style.pagination}>
+          {currentPage > 1 && (
+            <>
+              <button className={style.previousButton} onClick={handlePrevPage}>
+                Prev
+              </button>
+              <button className={style.pageNumber} onClick={ handlePageClick}>
+               { currentPage}
+              </button>
+            </>
+          )}
+          {currentPage < totalPages && (
+            <>
+              <button  className={style.nextButton} onClick={handleNextPage}>
+                Next 
+              </button>
+            </>
+          )}
+        </nav>
+      );
+    };
 
-export default Pagination;
+    export default Pagination;
+
+// return (
+// <nav>
+// <a
+// href="#"
+// className={`${style.previousButton} ${!isFirstPage ? style.showButton : ""}`}
+// onClick={handlePrevPage}
+// >
+// Previous Page
+// </a>
+// <div className={style.pageNumber}>
+// {[...Array(totalPages)].map((_, index) => (
+//   <a
+//     key={index}
+//     className={`${style.pageNumber} ${currentPage === index + 1 ? style.active : ""}`}
+//     href="#"
+//     onClick={() => handlePageClick(index + 1)}
+//   >
+//     {index + 1}
+//   </a>
+// ))}
+// </div>
+// <a
+// href="#"
+// className={`${style.nextButton} ${!isLastPage ? style.showButton : ""}`}
+// onClick={handleNextPage}
+// >
+// Next Page
+// </a>
+// </nav>
+// )
+
